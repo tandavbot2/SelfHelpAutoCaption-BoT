f0ea46400f1210e3fbc2efb01a3e13609a3e83d5
MR STRATEGIST <140485849+tandavbot@users.noreply.github.com>
2023-08-03 00:49:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,12 +47,24 @@ def edit_caption(bot, update: pyrogram.types.Message):
           try: update.edit(custom_caption.format(file_name=motech.file_name))
           except pyrogram.errors.FloodWait as FloodWait:
               asyncio.sleep(FloodWait.value)
-              # update.edit(custom_caption.format(file_name=motech.file_name, mote))
-             update.edit(custom_caption.format(motech.file_name, mote))
-
+              update.edit(custom_caption.format(motech.file_name, mote))
       except pyrogram.errors.MessageNotModified: pass 
   else:
       return
+# @AutoCaptionBot.on_message(pyrogram.filters.channel)
+# def edit_caption(bot, update: pyrogram.types.Message):
+#   if os.environ.get("custom_caption"):
+#       motech, _ = get_file_details(update)
+#       try:
+#           try: update.edit(custom_caption.format(file_name=motech.file_name))
+#           except pyrogram.errors.FloodWait as FloodWait:
+#               asyncio.sleep(FloodWait.value)
+#               # update.edit(custom_caption.format(file_name=motech.file_name, mote))
+#              update.edit(custom_caption.format(motech.file_name, mote))
+
+#       except pyrogram.errors.MessageNotModified: pass 
+#   else:
+#       return
     
 def get_file_details(update: pyrogram.types.Message):
   if update.media:
